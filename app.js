47024eef267aa53b66c47c2527da9e42babac5ff
PauloEsau1 <pauloesau1696@hotmail.com>
2021-05-20 22:02:19 -0500

    testTerminado

--- app.js
+++ app.js
@@ -6,26 +6,28 @@
            * pendiente
          )
 */
-const fs = require('fs')
-const tareas ={
-    path: 'tareas.json',
-    leer:function(){
-        const tareas = fs.readFileSync(this.path)
-    }
-}
+const fs = require('fs');
+path = "tareas.json";
 
-exports.leerTareas = function () {
-    const tareas = fs.readFileSync(this.path, 'utf-8');
+let leerTareas = function () {
+    let listado = fs.readFileSync(path,'utf-8');
     return JSON.parse(listado);
 }
 
-exports.agregarTarea = function (tarea) {
-    
-    tareas.push(tarea);
+let agregarTarea = function (tarea) {
+    let Agregatareas = leerTareas();
+    Agregatareas.push(tarea);
+    let tradu = JSON.stringify(Agregatareas);
+    fs.writeFileSync(path,tradu,'utf-8');
+    return tradu;
 
 }
 
-exports.filtrarTareasPorEstado = function (estado) {
+let filtrarTareasPorEstado = function (estado) {
+    let tareas = leerTareas();
     let EstadoTareas= tareas.filter(item=>item.status === estado);
          return EstadoTareas;
 }
+exports.leerTareas = leerTareas;
+exports.agregarTarea = agregarTarea;
+exports.filtrarTareasPorEstado = filtrarTareasPorEstado;
